39f317c13caeaa23936a712aa12fb7b7ffea3a76
duanjiawei <554343346@qq.com>
2020-05-13 00:07:54 +0800

    添加测试方法

---
+++ src/main/java/org/juhewu/email/JavaMailSendersImplTest.java
@@ -0,0 +1,14 @@
+package org.juhewu.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaMailSendersImplTest {
+    @Test
+ public void send(){
+
+ }
+}
--- src/main/java/org/juhewu/email/Test.java
+++ src/main/java/org/juhewu/email/Test.java
@@ -1,15 +1,56 @@
 package org.juhewu.email;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author duanjw
  */
+@Slf4j
 public class Test {
     public static void main(String[] args) {
-        MailAccount mailAccount = new MailAccount().setId("1");
+        Scanner s = new Scanner(System.in);
+        s.nextInt();
+        long time4 = System.currentTimeMillis();
+        List<MailAccount> mailAccounts = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            MailAccount mailAccount = new MailAccount().setId(i+"").setHost("smtp.163.com").setUsername("18630070626").setPassword("CBTAWAUHZZXLVQLJ");
+            mailAccounts.add(mailAccount);
+        }
         MailAccountRepository mailAccountRepository = new InMemoryMailAccountRepository();
-        mailAccountRepository.add(mailAccount);
-
+        mailAccountRepository.add(mailAccounts);
         JavaMailSenders javaMailSenders = new JavaMailSendersImpl(mailAccountRepository);
-        System.out.println(javaMailSenders.get("1"));
+        long time5 = System.currentTimeMillis();
+        log.info("初始化账户用时：{}", time5-time4);
+        for (int i = 0; i < 1000000; i++) {
+            long time1 = System.currentTimeMillis();
+            JavaMailSender javaMailSender = javaMailSenders.get(i+"");
+            long time2 = System.currentTimeMillis();
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setFrom("18630070626@163.com");
+
+            message.setTo("554343346@qq.com");
+
+            message.setSubject("it is a test for spring boot");
+            message.setText("你好，我是小黄，我正在测试发送邮件。");
+            javaMailSender.createMimeMessage();
+//            javaMailSender.send(message);
+            long time3 = System.currentTimeMillis();
+
+//            log.info("创建发送器{}用时：{}",javaMailSender, time2-time1);
+//            log.info("发送短信用时：{}", time3-time2);
+        }
+        long time6 = System.currentTimeMillis();
+        log.info("共共用时：{}", time6-time5);
+         s = new Scanner(System.in);
+        s.nextInt();
     }
+
+
 }
